32ecb21ff4f5e8a414eb303d67f5b9dc32e5957b
Jaspreet Bal <47026998+preet3112@users.noreply.github.com>
2024-08-10 14:32:19 -0400

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,7 @@
+#Add Comments with hashtag
+''' Add Multiline comments with 3-quotes'''
+print("Hello World")
+
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+print(a + b)
